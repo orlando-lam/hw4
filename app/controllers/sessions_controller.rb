3bba06691e19cb5605c7786e474d544046d0a189
orlando-lam <orlando.lam@kellogg.northwestern.edu>
2022-05-31 21:33:40 +0000

    v1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+        
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Logged Out"
+    redirect_to "sessions/new"
   end
 end
   
