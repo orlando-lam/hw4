a2518b8ceb9f3810296df4171f545c13ece3811c
orlando-lam <orlando.lam@kellogg.northwestern.edu>
2022-06-08 11:21:01 +0000

    update5

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,7 +22,7 @@ class SessionsController < ApplicationController
   def destroy
     session["user_id"] = nil
     flash["notice"] = "Logged Out"
-    redirect_to "/login"
+    redirect_to "/"
   end
 end
   
